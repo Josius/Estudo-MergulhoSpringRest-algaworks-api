e4ab6c3ef50f7be3a0fc54c52ebbc835cd72525b
Josius <josius.amaro@gmail.com>
2023-01-19 18:54:11 -0300

    aula 2-4

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # MergulhoSpringRest-algaworks
+Visual Studio Code v.1.74.3
+PostgreSQL v.15.1
+Java v.17.0.5 LTS
 
-JPA - 
+JPA - Biblioteca para ajudar a trabalhar com jakarta.persistance.api
+
 JAKARTA EE - Especificação contendo várias outras especificações
+jakarta.persistance.api - dependência transitiva, especificação
+
+HIBERNATE - Implementação de um especificação do jakarta.persistance.api
--- src/main/java/com/jram/api/jramapi/entitiy/Cliente.java
+++ src/main/java/com/jram/api/jramapi/entitiy/Cliente.java
@@ -1,12 +1,23 @@
 package com.jram.api.jramapi.entitiy;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
     
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
